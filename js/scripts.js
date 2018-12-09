b4a1a6069647e32cf8518eae1770d1c4daaef3df
Kevin Green <kevin.e.green@hotmail.com>
2018-12-08 23:59:59 -0600

    Site

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+ /* ================================
+            Preloader
+================================*/
+ $(window).on('load', function () { // makes sure site is loaded
+     $('#status').fadeOut();
+     $('#preloader').delay(350).fadeOut('slow');
+ });
+
+ /* ================================
+            Preloader
+================================*/
+ $(function () { // Short form on ready method
+     $("#team-members").owlCarousel({
+         items: 2,
+         autoplay: true,
+         smartSpeed: 1000,
+         loop: true,
+         autoplayHoverPause: true,
+         nav: true,
+         dots: false,
+         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+     });
+ });
+
+ /* ================================
+            Progress Bars
+================================*/
+ $(function () {
+     $("#progress-elements").waypoint(function () {
+         $(".progress-bar").each(function () {
+             $(this).animate({
+                 width: $(this).attr("aria-valuenow") + "%"
+             }, 1000);
+         });
+
+         this.destroy();
+     }, {
+         offset: 'bottom-in-view'
+     });
+ });
+
+ /* ================================
+            Responsive Tabs
+================================*/
+$(function(){
+    $("#services-tabs").responsiveTabs({
+        animation: 'slide'
+    });
+});
