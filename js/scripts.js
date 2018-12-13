88bc1c0f24a7268d7ba249c86bbff02dffc28e40
Kevin Green <kevin.e.green@hotmail.com>
2018-12-12 23:13:19 -0600

    Added Stats

--- js/scripts.js
+++ js/scripts.js
@@ -101,3 +101,12 @@
          navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
      });
  });
+ /* =========================================
+                Stats
+ ============================================ */
+$(function(){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
